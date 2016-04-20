2c3a80f100803d6f54b632937320e3574b00961a
git <cyfer2002@gmail.com>
2016-04-20 16:15:22 +0200

    Ajout de la page d'accueil et de la page contact. Création du Footer et du Header. Paramétrage de l'envoie de mail à faire sur page contact

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var title = "Smart'Dogs"
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('accueil', { title: title });
 });
 
+router.get('/contact', function(req, res, next) {
+  res.render('contact', {title: title});
+});
 module.exports = router;
